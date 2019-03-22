c179596dbf1882f34693b1182d79aad0b7ee7f39
rakeshlahre <rakeshlahre830@gmail.com>
2019-03-22 14:23:55 +0530

    db url change

--- src/db.js
+++ src/db.js
@@ -6,7 +6,7 @@ const promise = require("bluebird"),
     }
   },
   pgp = require("pg-promise")(initOptions),
-  db = pgp("postgres://rakesh:rakesh@localhost:5432/chatapp");
+  db = pgp("postgres://jysyoqkwnynmom:9c053a160476b794bee3f3cb7c698ea5b4001b3a00df5987c4b2c5b0ab61a161@ec2-174-129-10-235.compute-1.amazonaws.com:5432/d46uaatquvi7bq");
 
 module.exports = {
   users: require("./models/users")(db),
